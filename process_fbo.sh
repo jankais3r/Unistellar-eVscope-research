0f0ff03c11be6bc7f81b7bd397a8fdb9a994c355
Jan Kaiser <2159464+jankais3r@users.noreply.github.com>
2020-05-09 22:08:18 +0200

    Add files via upload

--- process_fbo.sh
+++ process_fbo.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 for f in ~/Desktop/evscope/media_rw/files/*
 do
-	echo "Processing $f file..."
+	echo "Processing $(basename $f) file..."
 	cat "$f" | tail -c +263 | magick -size 1304x976 -depth 16 "GRAY:-" $f.png || cat "$f" | magick -size 992x976 -depth 16 "GRAY:-" $f.png
 done
